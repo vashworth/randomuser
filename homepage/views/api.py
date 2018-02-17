7666f6a37f8833e0858cb2400b1ba7c85ad54c22
Victoria Ashworth <toriaknishimoto@gmail.com>
2018-02-17 15:34:33 -0700

    fixed json

--- homepage/views/api.py
+++ homepage/views/api.py
@@ -2,6 +2,7 @@ from homepage import models as m
 from django_mako_plus import view_function
 import json
 from django.forms.models import model_to_dict
+from django.core import serializers
 
 @view_function
 def process_request(request):
@@ -11,7 +12,7 @@ def process_request(request):
             #check for api key
             test = m.APIKey.objects.get(key=request.GET.get('key'))
 
-            #get person by id
+            #get person by googleID
             person = m.Person.objects.get(googleID=request.GET.get('googleID'))
 
             #return as json
@@ -19,10 +20,11 @@ def process_request(request):
             json_response = json.dumps(person_dict)
 
         except m.Person.DoesNotExist:
+            #get all people
             people = m.Person.objects.all()
             #return as json
-            people_dict = model_to_dict( people )
-            json_response = json.dumps(people_dict)
+            people_json = serializers.serialize('json', people)
+            json_response = people_json
 
         except m.APIKey.DoesNotExist:
             json_response = "Invalid api key"
@@ -71,7 +73,7 @@ def process_request(request):
             p.img_url = request.GET.get('img_url')
             p.positions = request.GET.get('positions')
 
-            if (p.first_name is not None):
+            if (p.googleID is not None):
                 p.save()
 
     context = {
